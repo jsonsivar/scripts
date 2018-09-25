3a41cb14d3e321aaf43f9a2a0a48de8882e54215
Janison Sivarajah <jsonsivar@gmail.com>
2018-09-25 02:36:57 -0400

    Update start-parity.sh

--- start-parity.sh
+++ start-parity.sh
@@ -1,2 +1,5 @@
 # tracing not enabled, add --tracing=on and expose tracing in the apis if needed
 nohup parity --chain=mainnet --identity=parity-01 --jsonrpc-apis=web3,net,eth,pubsub,parity,parity_pubsub,rpc,shh,shh_pubsub --jsonrpc-port=7010 --jsonrpc-interface=none --logging=6 --log-file=parity.log &
+
+# use the following command to use geth (if installed) for console access to parity
+# geth attach ~/.local/share/io.parity.ethereum/jsonrpc.ipc
